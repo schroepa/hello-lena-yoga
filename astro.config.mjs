a5d77a057783d7777403ac982ac01a49b16dfb74
Patrick Schröder <schroepa1981@gmail.com>
2026-06-03 10:16:31 +0200

    feat: Vercel adapter, output static, vercel.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
   integrations: [react()],
   i18n: {
     defaultLocale: 'de',
